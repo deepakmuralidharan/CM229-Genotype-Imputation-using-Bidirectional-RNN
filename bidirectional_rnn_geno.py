de2a54c0aa6d3339859a33ed38922028ba668ae1
Deepak Muralidharan <deepakmuralidharan2308@gmail.com>
2016-05-19 22:35:01 -0700

    Made batch sizes different for test and validation

--- bidirectional_rnn_geno.py
+++ bidirectional_rnn_geno.py
@@ -13,22 +13,30 @@ import numpy as np
 import time
 import sys
 import math
+from sklearn.metrics import f1_score
 
 # Parameters
-learning_rate = 0.001
+learning_rate = 0.01
 training_iters = 100000
 batch_size = 56 # number of rows in the genotype dataset
 display_step = 10 # for display purposes
 
 # Network Parameters
 n_input = 1 # dimension of input data (for genotype data-- since value is binary n_input = 1)
-n_steps = 49 # number of columns in the data matrix
+n_steps = 15 # number of columns in the data matrix
 n_hidden = 5 # hidden layer -- hyperparameter -- values can range between 1-10
 n_classes = 1 # dimension of outout (for genotype data modelling - output is 1 or 0)
-max_epochs = 1000 # maximum number of epochs we want the training to run for
+max_epochs = 50 # maximum number of epochs we want the training to run for
 
 # loading the data file
-data = np.loadtxt('/Users/deepakmuralidharan/Documents/Bidirectional-LSTM/data/train_data.txt',delimiter=',')
+data = np.loadtxt('/Users/deepakmuralidharan/Documents/Bidirectional-LSTM/data/geno_loc_1.txt',delimiter=',')
+xor_data = np.loadtxt('/Users/deepakmuralidharan/Documents/Bidirectional-LSTM/data/randi.txt',delimiter=',')
+test_data = xor_data[1736:2184, 00:15]
+print test_data.shape
+test_label = data[1736:2184, 01:16]
+print test_label.shape
+data = data[0:2184, 00:16]
+valid_len = 448
 
 # tf Graph input
 x = tf.placeholder("float", [None, n_steps, n_input]) # [batch size, number of steps, input dimension]
@@ -63,7 +71,7 @@ def geno_iterator(raw_data, batch_size, num_steps):
       x = raw_data[i * batch_size: (i + 1) * batch_size, 0:num_steps] # giving the entire range as time steps
       y = raw_data[i * batch_size: (i + 1) * batch_size, 1:(num_steps + 1)]
       yield (x,y)
-
+'''
 def BiRNN(_X, _istate_fw, _istate_bw, _weights, _biases, _batch_size, _seq_len):
 
     # BiRNN requires to supply sequence_length as [batch_size, int64]
@@ -81,9 +89,9 @@ def BiRNN(_X, _istate_fw, _istate_bw, _weights, _biases, _batch_size, _seq_len):
 
     # Define lstm cells with tensorflow
     # Forward direction cell
-    lstm_fw_cell = rnn_cell.BasicLSTMCell(n_hidden, forget_bias=1.0)
+    lstm_fw_cell = rnn_cell.BasicLSTMCell(n_hidden, forget_bias=1)
     # Backward direction cell
-    lstm_bw_cell = rnn_cell.BasicLSTMCell(n_hidden, forget_bias=1.0)
+    lstm_bw_cell = rnn_cell.BasicLSTMCell(n_hidden, forget_bias=1)
     # Split data because rnn cell needs a list of inputs for the RNN inner loop
     _X = tf.split(0, n_steps, _X) # n_steps * (batch_size, n_hidden)
 
@@ -97,14 +105,47 @@ def BiRNN(_X, _istate_fw, _istate_bw, _weights, _biases, _batch_size, _seq_len):
     # Get inner loop last output
     output = [tf.matmul(o, _weights['out']) + _biases['out'] for o in outputs]
     return output
+'''
 
-pred = BiRNN(x, istate_fw, istate_bw, weights, biases, batch_size, n_steps)
+def BiRNN(_X, _istate_fw, _istate_bw, _weights, _biases):
+
+     # input shape: (batch_size, n_steps, n_input)
+    _X = tf.transpose(_X, [1, 0, 2])  # permute n_steps and batch_size
+    # Reshape to prepare input to hidden activation
+    _X = tf.reshape(_X, [-1, n_input]) # (n_steps*batch_size, n_input)
+    # Linear activation
+    _X = tf.matmul(_X, _weights['hidden']) + _biases['hidden']
+
+    # Define lstm cells with tensorflow
+    # Forward direction cell
+    lstm_fw_cell = rnn_cell.BasicLSTMCell(n_hidden, forget_bias=1.0)
+    # Backward direction cell
+    lstm_bw_cell = rnn_cell.BasicLSTMCell(n_hidden, forget_bias=1.0)
+    # Split data because rnn cell needs a list of inputs for the RNN inner loop
+    _X = tf.split(0, n_steps, _X) # n_steps * (batch_size, n_hidden)
+
+    # Get lstm cell output
+    outputs = rnn.bidirectional_rnn(lstm_fw_cell, lstm_bw_cell, _X,
+                                            initial_state_fw=_istate_fw,
+                                            initial_state_bw=_istate_bw)
+
+    # Linear activation
+    # Get inner loop last output
+    output = [tf.matmul(o, _weights['out']) + _biases['out'] for o in outputs]
+    return output
+
+
+#pred = BiRNN(x, istate_fw, istate_bw, weights, biases, batch_size, n_steps)
+pred = BiRNN(x, istate_fw, istate_bw, weights, biases)
+pred = tf.concat(1, pred)
 
 # Define loss function and optimizer
-pred = tf.concat(1, pred)
+
 _y  = tf.squeeze(y,[2])
 cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(pred, _y)) # Softmax loss
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost) # Adam Optimizer
+#cost_valid = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(pred, _y)) # for test
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
 
 # Evaluate model
 #correct_pred = tf.equal(tf.argmax(pred,1), tf.argmax(y,1))
@@ -118,6 +159,8 @@ with tf.Session() as sess:
 
     sess.run(init)
 
+    best_val_epoch = float("inf")
+
     for epoch in xrange(max_epochs):
 
         total_loss = []
@@ -128,18 +171,17 @@ with tf.Session() as sess:
         start = time.time()
         for step, (batch_xs, batch_ys) in enumerate(
           geno_iterator(data, batch_size, n_steps)):
+
           batch_xs = np.reshape(batch_xs,[batch_size, n_steps, n_input])
           batch_ys = np.reshape(batch_ys,[batch_size, n_steps, n_input])
-          #print batch_xs.shape
-          #print batch_ys.shape
 
           sess.run(optimizer, feed_dict={x: batch_xs, y: batch_ys,
                                          istate_fw: np.zeros((batch_size, 2*n_hidden)),
                                          istate_bw: np.zeros((batch_size, 2*n_hidden))})
-          predicted,loss,ground_truth = sess.run([pred,cost,_y], feed_dict={x: batch_xs, y: batch_ys,
+
+          predicted, loss, ground_truth = sess.run([pred, cost, _y], feed_dict={x: batch_xs, y: batch_ys,
                                            istate_fw: np.zeros((batch_size, 2*n_hidden)),
                                            istate_bw: np.zeros((batch_size, 2*n_hidden))})
-
 
           total_loss.append(loss)
           if verbose and step % verbose == 0:
@@ -149,16 +191,52 @@ with tf.Session() as sess:
 
         if verbose:
             sys.stdout.write('\r')
-        print (1/(1+np.exp(-predicted[0,0:11])))
-        print (ground_truth[0,0:11])
+        #print (1/(1+np.exp(-predicted[0,0:11])))
+        #print (ground_truth[0,0:11])
 
         print 'Training loss: {}'.format(np.mean(total_loss))
 
+        test_data = np.reshape(test_data,[valid_len, n_steps, n_input])
+        test_label = np.reshape(test_label,[valid_len, n_steps, n_input])
+        start = time.time()
+
+        validation_loss = sess.run(cost, feed_dict={x: test_data, y: test_label,
+                                                                 istate_fw: np.zeros((valid_len, 2*n_hidden)),
+                                                                 istate_bw: np.zeros((valid_len, 2*n_hidden))})
+
+        #print 'Run time: {}'.format(time.time() - start)
+        print 'Validation loss: {}'.format(validation_loss)
+
+        if validation_loss > best_val_epoch:
+            print "Breaking out of algorithm..."
+            break
+
+        best_val_epoch = validation_loss
+
+
+
     print "Optimization Finished!"
+
+    test_data = np.reshape(test_data,[valid_len, n_steps, n_input])
+    test_label = np.reshape(test_label,[valid_len, n_steps, n_input])
+    print test_data.shape
+
+    y_predicted, testing_loss, y_true = sess.run([pred, cost, _y], feed_dict={x: test_data, y: test_label,
+                                                             istate_fw: np.zeros((valid_len, 2*n_hidden)),
+                                                             istate_bw: np.zeros((valid_len, 2*n_hidden))})
+
+
+    print 'Testing loss: {}'.format(testing_loss)
+    y_predicted = 1/(1+np.exp(-y_predicted))
+
+    print 'F1-score: {}'.format(f1_score(y_true, np.around(y_predicted)), average = 'macro')
+
+
+
     # Calculate accuracy for 128 mnist test images
-    #test_len = 128
-    #test_data = mnist.test.images[:test_len].reshape((-1, n_steps, n_input))
-    #test_label = mnist.test.labels[:test_len]
+    #valid_len = 128
+    #test_data = mnist.test.images[:valid_len].reshape((-1, n_steps, n_input))
+    #test_label = mnist.test.labels[:valid_len]
     #print "Testing Accuracy:", sess.run(accuracy, feed_dict={x: test_data, y: test_label,
-                                                             #istate_fw: np.zeros((test_len, 2*n_hidden)),
-                                                             #istate_bw: np.zeros((test_len, 2*n_hidden))})
+                                                             #istate_fw: np.zeros((valid_len, 2*n_hidden)),
+                                                             #istate_bw: np.zeros((valid_len, 2*n_hidden))})
